77d097a258764be1141606e4e53280aa58cd33a9
Szabó Dániel <60423403+Danel-adatb@users.noreply.github.com>
2021-10-04 11:30:21 +0200

    Add files via upload

---
+++ first/src/main/java/hu/me/iit/webalk/first/ArticleDto.java
@@ -0,0 +1,33 @@
+package hu.me.iit.webalk.first;
+
+public class ArticleDto {
+	private String author;
+	private int page;
+	private String title;
+	private int pages;
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+}
--- first/src/main/java/hu/me/iit/webalk/first/mainContoller.java
+++ first/src/main/java/hu/me/iit/webalk/first/mainContoller.java
@@ -1,20 +1,61 @@
 package hu.me.iit.webalk.first;
 
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.ArrayList;
+import java.util.List;
 
-@Controller
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class mainContoller {
+	private final List<ArticleDto> articles = new ArrayList<>();
 	
-	@RequestMapping(path="/", produces= MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	ResponseDto gyoker() {
-		ResponseDto rdto = new ResponseDto();
+	
+	@GetMapping(path="articles/", produces=MediaType.APPLICATION_JSON_VALUE)
+	List<ArticleDto> allArticles() {
+		return articles;
+	}
+	
+	@PostMapping(path="articles/")
+	void newArticle(@RequestBody ArticleDto articlesDto) {
+		articles.add(articlesDto);
+	}
+	
+	@PutMapping(path="articles/{id}")
+	void ReplaceArticle(@PathVariable("id") String id, @RequestBody ArticleDto articleDto) {
+		int num = -1;
+		for (int i = 0; i < articles.size(); i++) {
+			if(articles.get(i).getTitle().equals(id)) {
+				num = i;
+				break;
+			}
+		}
 		
-		rdto.setId(123);
-		rdto.setMessage("Hello World!");
-		return rdto;
+		if(num != -1) {
+			ArticleDto foundArticles = articles.get(num);
+			foundArticles.setAuthor(articleDto.getAuthor());
+			foundArticles.setPages(articleDto.getPages());
+		}
+	}
+	
+	@DeleteMapping(path="articles/{id}")
+	void deleteArticle(@PathVariable("id") String id) {
+		int num = -1;
+		for (int i = 0; i < articles.size(); i++) {
+			if(articles.get(i).getTitle().equals(id)) {
+				num = i;
+				break;
+			}
+		}
+		
+		if(num != -1) {
+			articles.remove(num);
+		}
 	}
 }
