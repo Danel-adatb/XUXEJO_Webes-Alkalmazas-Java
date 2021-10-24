185bb6d8755d20feeff0b672c8fc5a7fa3745e5c
Szabó Dániel <60423403+Danel-adatb@users.noreply.github.com>
2021-10-24 17:33:50 +0200

    Update MainController.java

--- dependency/src/main/java/hu/me/iit/webalk/dependency/MainController.java
+++ dependency/src/main/java/hu/me/iit/webalk/dependency/MainController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+  private Service service;
+
+    @Autowired
+    public void setService(Service service) {
+        this.service = service;
+    }
 
 
 }
